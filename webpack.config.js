05c6d2c8c4a16c517e6cb1000acbfadc8f006612
Alvin Kwong <akwong372@gmail.com>
2019-03-11 11:48:19 -0700

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/components/Index.jsx',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './public/dist'
+  }
+};
