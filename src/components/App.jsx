662a29c25cbebb4c1ca018fdd56c399d655c74dd
akwong372 <akwong372@gmail.com>
2020-08-20 16:04:04 -0700

    made top arrow look better, added keeper project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,53 +8,61 @@ class App extends React.Component {
     super(props);
     this.state = {
       devIcons: {
-        HTML5: <i className="icon-html5-alt"/>,
-        CSS3: <i className="icon-css3-alt"/>,
-        Bootstrap: <i className="icon-bootstrap"/>,
-        JavaScript: <i className="icon-javascript-alt"/>,
-        jQuery: <i className="icon-jquery"/>,
-        AWS: <i class="icon-aws"/>,
-        Node: <i className="icon-nodejs"/>,
-        React: <i className="icon-reactjs"/>,
-        nginx: <i className="icon-nginx-alt"/>,
-        MongoDB: <i className="icon-mongodb"/>,
-        Redis: <i className="icon-redis"/>
+        HTML5: <i className="icon-html5-alt" />,
+        CSS3: <i className="icon-css3-alt" />,
+        Bootstrap: <i className="icon-bootstrap" />,
+        JavaScript: <i className="icon-javascript-alt" />,
+        jQuery: <i className="icon-jquery" />,
+        AWS: <i class="icon-aws" />,
+        Node: <i className="icon-nodejs" />,
+        React: <i className="icon-reactjs" />,
+        nginx: <i className="icon-nginx-alt" />,
+        MongoDB: <i className="icon-mongodb" />,
+        Redis: <i className="icon-redis" />
       },
-      projects: [{
-        title: 'A Video Player Microservice',
-        description: 'Video player microservice for a vimeo clone designed with service oriented architecture. A responsive front end built with React, Bootstrap, and some custom CSS. The back end was built with Express/Node and deployed to AWS EC2, load balanced with nginx, and cached responses with Redis. Service could handle up to 3000 requests per second with < 1% error rate and ~120ms response rate while querying a database of ~10 million entries.',
-        technologies: ['React', 'JavaScript', 'Bootstrap', 'Node', 'nginx', 'MongoDB', 'Redis', 'AWS'],
-        link: 'https://www.youtube.com/watch?v=3vibmxLCIvs&feature=youtu.be',
-        image: 'https://github.com/sdc-windsor/video-player/blob/master/VidNavSm.gif?raw=true'
-      },
-      {
-        title: 'A Simon Game Clone',
-        description: 'A memory game based on the Simon toy. The player matches color/sound cues in sequence Written mostly in Javascript and without jQuery as a way to improve my ability with vanilla Javascript.',
-        technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript'],
-        link: 'https://codepen.io/niceboat111/full/BxjPMy/',
-        image: 'https://i.imgur.com/KrH4bgU.png'
-      },
-      {
-        title: 'A Twitch Stream Status Viewer',
-        description: `This is a simple app for keeping track of a preset list of channels from the Twitch website. Online channels will display their status message while offline channels will display "offline". Users can toggle between seeing only online channels, offline channels, or both. The status and portrait for each channel are accessed through a modified link to Twitch's API using jQuery's AJAX calls.`,
-        technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'jQuery'],
-        link: 'https://codepen.io/niceboat111/full/MGWYKg/',
-        image: 'https://i.imgur.com/QZPxk0u.png'
-      },
-      {
-        title: 'A Pomodoro Timer',
-        description: `A timer to be used with the Pomodoro Technique. It has a "work" timer and a "break" timer and both can be adjusted in 1 minute increments. An alert sound is played when the "work" timer runs out, and once again when the "break" timer runs out.`,
-        technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'jQuery'],
-        link: 'https://codepen.io/niceboat111/full/pLbNwN/',
-        image: 'https://i.imgur.com/giQtdYN.png'
-      },
-      {
-        title: 'A Survey Form',
-        description: 'A page meant to be an example of a survey form. It features the usage of various input fields including checkboxes, email, and numbers. Also features placeholder text and checks if required fields are filled in.',
-        technologies: ['HTML5', 'CSS3', 'Bootstrap'],
-        link: 'https://akwong372.github.io/survey-form/',
-        image: 'https://i.imgur.com/SKYiBQH.png'
-      }],
+      projects: [
+        {
+          title: 'Keeper',
+          description: 'A clone of Google Keep, but also featuring the ability to log in and out using Passport.js. The ability to log in using a Google account through OAuth 2.0 was also added. User accounts and notes are stored using MongoDB Atlas and the app is currently deployed to Heroku.',
+          technologies: ['React', 'Node', 'MongoDB'],
+          link: 'https://keeper-app-1.herokuapp.com/',
+          image: 'https://i.imgur.com/QkTxCej.gif'
+        },
+        {
+          title: 'A Video Player Microservice',
+          description: 'Video player microservice for a vimeo clone designed with service oriented architecture. A responsive front end built with React, Bootstrap, and some custom CSS. The back end was built with Express/Node and deployed to AWS EC2, load balanced with nginx, and cached responses with Redis. Service could handle up to 3000 requests per second with < 1% error rate and ~120ms response rate while querying a database of ~10 million entries.',
+          technologies: ['React', 'Bootstrap', 'Node', 'nginx', 'MongoDB', 'Redis', 'AWS'],
+          link: 'https://www.youtube.com/watch?v=3vibmxLCIvs&feature=youtu.be',
+          image: 'https://github.com/sdc-windsor/video-player/blob/master/VidNavSm.gif?raw=true'
+        },
+        {
+          title: 'A Simon Game Clone',
+          description: 'A memory game based on the Simon toy. The player matches color/sound cues in sequence Written mostly in Javascript and without jQuery as a way to improve my ability with vanilla Javascript.',
+          technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript'],
+          link: 'https://codepen.io/niceboat111/full/BxjPMy/',
+          image: 'https://i.imgur.com/KrH4bgU.png'
+        },
+        {
+          title: 'A Twitch Stream Status Viewer',
+          description: `This is a simple app for keeping track of a preset list of channels from the Twitch website. Online channels will display their status message while offline channels will display "offline". Users can toggle between seeing only online channels, offline channels, or both. The status and portrait for each channel are accessed through a modified link to Twitch's API using jQuery's AJAX calls.`,
+          technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'jQuery'],
+          link: 'https://codepen.io/niceboat111/full/MGWYKg/',
+          image: 'https://i.imgur.com/QZPxk0u.png'
+        },
+        {
+          title: 'A Pomodoro Timer',
+          description: `A timer to be used with the Pomodoro Technique. It has a "work" timer and a "break" timer and both can be adjusted in 1 minute increments. An alert sound is played when the "work" timer runs out, and once again when the "break" timer runs out.`,
+          technologies: ['HTML5', 'CSS3', 'Bootstrap', 'JavaScript', 'jQuery'],
+          link: 'https://codepen.io/niceboat111/full/pLbNwN/',
+          image: 'https://i.imgur.com/giQtdYN.png'
+        },
+        {
+          title: 'A Survey Form',
+          description: 'A page meant to be an example of a survey form. It features the usage of various input fields including checkboxes, email, and numbers. Also features placeholder text and checks if required fields are filled in.',
+          technologies: ['HTML5', 'CSS3', 'Bootstrap'],
+          link: 'https://akwong372.github.io/survey-form/',
+          image: 'https://i.imgur.com/SKYiBQH.png'
+        }],
       contactLinks: [
         {
           link: 'https://www.linkedin.com/in/alvin-kwong-8b33ab47/',
@@ -100,6 +108,8 @@ class App extends React.Component {
       <ContactLinks key={'link' + i} link={link.link} faIcon={link.faIcon} />
     );
 
+    const currentYear = new Date().getFullYear();
+
     return (
       <div>
         <Navbar />
@@ -117,7 +127,7 @@ class App extends React.Component {
             <h1 className="title">about</h1>
             <p className="about-text">I'm a full stack software engineer from San Francisco, California with a background
         <br />
-        in JavaScript and experience building, deploying, and scaling web applications.
+              in JavaScript and experience building, deploying, and scaling web applications.
         <br />
               When I'm not working or studying, I might be soldering together a Nerf blaster or looking up nonsense on the internet.</p>
           </section>
@@ -146,7 +156,7 @@ class App extends React.Component {
 
         <footer className="footer-style">
           <p>Created by
-      <a href="https://github.com/akwong372" target="_blank"> Alvin Kwong</a> © 2018</p>
+      <a href="https://github.com/akwong372" target="_blank"> Alvin Kwong</a> © {currentYear}</p>
         </footer>
       </div>
     );
